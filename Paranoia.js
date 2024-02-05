db65028b74bac7c93e82324b70f5ee2bfcbc8cbb
SvetaTurko <83105911+SvetaTurko@users.noreply.github.com>
2024-02-04 20:51:28 -0700

    Add files via upload
    Signed-off-by: SvetaTurko <83105911+SvetaTurko@users.noreply.github.com>

---
+++ Paranoia.js
@@ -0,0 +1,21 @@
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru"
+    }
+];
+
+var validEmails = arr.filter(obj => /^[a-z0-9]+\.[a-z0-9]+@(gmail|yahoo)\.com$/.test(obj.email));
+
+console.log(validEmails);
